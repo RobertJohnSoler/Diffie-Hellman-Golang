7211cbf2c139ce9afef64d5ac3ee36078704137d
RobertJohnSoler <rsoler@hawk.iit.edu>
2024-12-22 23:37:36 -0600

    can now check prime numbers

--- main.go
+++ main.go
@@ -30,9 +30,44 @@ func genRandBigInt2(bit_len int) (*big.Int, error) {
 	return randBigInt, err
 }
 
+func primeCheck(number *big.Int) bool {
+	prime := false
+	s := 150
+
+	for i := 1; i <= s; i++ {
+		if number.Cmp(big.NewInt(4)) == 0 {
+			break
+		} else if (number.Cmp(big.NewInt(4))) < 0 {
+			prime = true
+			break
+		}
+
+		max := new(big.Int).Sub(number, big.NewInt(2))
+		Range := new(big.Int).Sub(max, big.NewInt(2))
+		random := rand.Reader
+		randrange, _ := rand.Int(random, Range)
+		// modded_randrange := new(big.Int).Mod(randrange, Range)
+		a := new(big.Int).Add(randrange, big.NewInt(2))
+		primecheck := new(big.Int)
+		primecheck.Exp(a, new(big.Int).Sub(number, big.NewInt(1)), number)
+		if primecheck.Cmp(big.NewInt(1)) != 0 {
+			prime = false
+			break
+		}
+		prime = true
+
+	}
+	return prime
+}
+
 func main() {
 	random1, _ := genRandBigInt1(128)
 	random2, _ := genRandBigInt2(128)
 	fmt.Println("First random is ", random1.String())
 	fmt.Println("Second random is ", random2.String())
+	for i := 0; i <= 30; i++ {
+		num := big.NewInt(int64(i))
+		fmt.Println(num, " is ", primeCheck(num))
+	}
+
 }
