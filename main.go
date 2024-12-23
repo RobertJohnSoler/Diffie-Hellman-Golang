0e1ab0521ea109ed0ab2cd33d8f1da6ffb15b29c
RobertJohnSoler <rsoler@hawk.iit.edu>
2024-12-22 22:35:21 -0600

    can now generate random big ints

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+)
 
-func main(){
-	fmt.Println("Hello World!")
+func genRandBigInt1(bit_len int) (*big.Int, error) {
+	byte_len := (bit_len + 7) / 8
+	rand_bytes := make([]byte, byte_len)
+	_, err := rand.Read(rand_bytes)
+	if err != nil {
+		return nil, err
+	}
+	randBigInt := new(big.Int)
+	randBigInt.SetBytes(rand_bytes)
+	return randBigInt, nil
+}
+
+func genRandBigInt2(bit_len int) (*big.Int, error) {
+	x := big.NewInt(2)
+	y := big.NewInt(int64(bit_len))
+	max := new(big.Int)
+	max.Exp(x, y, nil)
+	randBigInt, err := rand.Int(rand.Reader, max)
+	if err != nil {
+		return nil, err
+	}
+	return randBigInt, err
+}
+
+func main() {
+	random1, _ := genRandBigInt1(128)
+	random2, _ := genRandBigInt2(128)
+	fmt.Println("First random is ", random1.String())
+	fmt.Println("Second random is ", random2.String())
 }
